b7fedd59bc9a3e56ac85406110685f2939c297cc
Björn Bärtschi <linuxsatellite@gmail.com>
2020-05-13 21:41:13 +0200

    Create main.groovy

---
+++ main.groovy
@@ -0,0 +1,24 @@
+pipeline {
+  agent any
+  
+  parameters {
+            string(defaultValue: "NULL", description: 'Argument 1 (String Value)', name: 'argv_1')
+  }
+  
+  environment {
+    ARGV_1 = "${params.argv_1}"
+  }
+  
+  stages {
+    stage('Initial Stage') {
+      steps {
+        echo "Initial Stage"
+        deleteDir()
+      }
+    }
+    stage('Init Git') {
+      echo "Init Git"
+      echo "DEBUG: ARGV_1 = " + ARGV_1
+    }
+  }
+}
